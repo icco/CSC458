53548d060a2760eef8ef8cc1cf22e7a109f2eb1c
Nat Welch <nat@natwelch.com>
2010-04-03 16:26:13 -0700

    work.

--- 1Lab/lab1.fs
+++ 1Lab/lab1.fs
@@ -17,6 +17,9 @@ let rec manysamples x =
    else
       (rand ()) + (manysamples (x - 1))
 
+
+
+// takes a unit -> int function
 let rec manysamples2 f x = if x = 0 then 0 else (manysamples2 f (x - 1)) + (f ())
 
 (*
@@ -24,23 +27,38 @@ let rec manysamplesarr f a =
   match a with
   | [] = 0
   | (x::xs) = (f x) + manysamplesarr f xs
+
+let newrand a _ = 
+   let y = r.NextDouble()
+   in
+      Array.fold (fun x xs -> if y < x then 0.0 else 1.0) 0.0 a
+;
+
 *)
 
-let newrand a = newrandr a (r.NextDouble())
-
 let rec newrandr a y = 
-  match a with
-  | [] = 0
-  | (x::xs) = if x < a
+   match a with
+   | [] -> 0
+   | (x::xs) -> if y < x then 0 else (1 + (newrandr xs y))
 
-
-
-
-let rec manysamplesarray f x = 0
+let newrand a () = newrandr a (r.NextDouble())
 
 printf "Compund: %f\n" (compound 2.0);;
 printf "Random: %d\n" (rand ());;
 printf "Many Samples: %d\n" (manysamples 5);;
 printf "Many Samples Generalized: %d\n" (manysamples2 (rand) 5);;
+(*
 printf "Random Generalized: %d\n"  manysamples2 (newrand [| 0.3; 0.9 |]) 5;;
+*)
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
+printf "%d\n" (newrand [ 0.3; 0.9 ] ()) ;;
 

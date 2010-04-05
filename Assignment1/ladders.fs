9138c1edefd036eace3df3dad7d384b0a8a1414d
Nat Welch <nat@natwelch.com>
2010-04-05 09:56:56 -0700

    Whee assignment 1 work

---
+++ Assignment1/ladders.fs
@@ -0,0 +1,31 @@
+(**
+ * Chutes and Ladders 
+ * @author Nat Welch
+ * @author Mark Gius
+ *)
+
+// I landed on position pos, where should I be?
+let gameBoard pos = 
+    match pos with
+        | 1 -> 38
+        | 4 -> 14
+        | 9 -> 31
+        | 16 -> 6
+        | 21 -> 42
+        | 28 -> 84
+        | 36 -> 44
+        | 47 -> 26
+        | 49 -> 11
+        | 51 -> 67
+        | 56 -> 53
+        | 62 -> 19
+        | 64 -> 60
+        | 71 -> 91
+        | 80 -> 100
+        | 87 -> 24
+        | 93 -> 73
+        | 96 -> 75
+        | 98 -> 78
+        | _ -> pos
+
+

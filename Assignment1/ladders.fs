a73a3854a412d6128c21bfe0bef3ee06cf6bfb81
Nat Welch <nat@natwelch.com>
2010-04-11 19:12:27 -0700

    turning matches into if stmts

--- Assignment1/ladders.fs
+++ Assignment1/ladders.fs
@@ -9,30 +9,30 @@ let r = System.Random()
 
 // I landed on position pos, where should I be?
 let pos p =
-    match p with
-        | 1 -> 38
-        | 4 -> 14
-        | 9 -> 31
-        | 16 -> 6
-        | 21 -> 42
-        | 28 -> 84
-        | 36 -> 44
-        | 47 -> 26
-        | 49 -> 11
-        | 51 -> 67
-        | 56 -> 53
-        | 62 -> 19
-        | 64 -> 60
-        | 71 -> 91
-        | 80 -> 100
-        | 87 -> 24
-        | 93 -> 73
-        | 96 -> 75
-        | 98 -> 78
-        | _ -> p
+   match p with
+      | 1 -> 38
+      | 4 -> 14
+      | 9 -> 31
+      | 16 -> 6
+      | 21 -> 42
+      | 28 -> 84
+      | 36 -> 44
+      | 47 -> 26
+      | 49 -> 11
+      | 51 -> 67
+      | 56 -> 53
+      | 62 -> 19
+      | 64 -> 60
+      | 71 -> 91
+      | 80 -> 100
+      | 87 -> 24
+      | 93 -> 73
+      | 96 -> 75
+      | 98 -> 78
+      | _ -> p
 
 let roll _ = r.Next (1,7)
-        
+
 (**
  * Player Class
  * 
@@ -41,8 +41,8 @@ let roll _ = r.Next (1,7)
  *)
 [<AbstractClass>]
 type Player () =
-    abstract shouldDouble : int -> int -> bool
-    abstract shouldTake : int -> int -> bool
+   abstract shouldDouble : int -> int -> bool
+   abstract shouldTake : int -> int -> bool
 
 
 (**
@@ -51,61 +51,58 @@ type Player () =
  * @author mgius
  *)
 type RecklessPlayer () =
-    inherit Player ()
-    override this.shouldDouble myPos hisPos =
-        myPos > hisPos
-
-    override this.shouldTake myPos hisPos =
-        true
+   inherit Player ()
+   override this.shouldDouble myPos hisPos = (myPos > hisPos)
+   override this.shouldTake myPos hisPos = true
 
 (**
  * Like the Reckless player, but only takes when he is ahead.
  * @author nwelch 
  *)
 type ThinkingPlayer () =
-    inherit Player ()
-    override this.shouldDouble myPos hisPos =
-        myPos > hisPos
-
-    override this.shouldTake myPos hisPos =
-        myPos > hisPos
+   inherit Player ()
+   override this.shouldDouble myPos hisPos = (myPos > hisPos)
+   override this.shouldTake myPos hisPos = (myPos > hisPos)
 
 (**
  * The game function....
- * 
- * Replaced most of my code with mgius'.
  *)
-let rec realRunGame gameState (p_one : Player) (p_two : Player) =
-    let (whoseTurn, doubles, cubeOwner, pos1, pos2) = gameState
+let rec realRunGame gameState ( p_one : Player ) ( p_two : Player ) =
+   let (whoseTurn, doubles, cubeOwner, pos1, pos2) = gameState
+   let move = roll ()
 
-    // Figure out what the new doubling value is
-    let newDoubles, newOwner =
-        match (cubeOwner = -1 || cubeOwner % 2 = whoseTurn % 2, p_one.shouldDouble pos1 pos2) with
-            | (true, true) -> if p_two.shouldTake pos2 pos1 then
-               (doubles + 1, whoseTurn + 1)
-            else
-               (doubles - 1, cubeOwner)
-            // Since we only care about true true, catch-all
-            | (_, _) -> (doubles, cubeOwner)
+   let newDoubles = (
+      if (cubeOwner = -1 || cubeOwner % 2 = whoseTurn % 2) && (p_one.shouldDouble pos1 pos2) then 
+         if p_two.shouldTake pos2 pos1 then (doubles + 1) else (doubles - 1)
+      else
+         doubles
+   )
 
-    let playerMod =
-        match whoseTurn % 2 with
-            | 0 ->  1.0
-            | 1 -> -1.0
-            | _ ->  0.0 // impossible
-        
-    match newDoubles, gameBoard (pos1 + (roll ())) with
-        | newDoubles, _ when newDoubles < doubles ->
-            // This is an indicator that playertwo rejected the bet
-            (pos1, pos2) ||> printfn "Rejected: %d %d"
-            2.0 ** float doubles * playerMod
-        | _, newPos when newPos >= 100 ->
-            (pos1, pos2) ||> printfn "Victory: %d %d"
-            2.0 ** float newDoubles * playerMod
-        | _, newPos ->
-            (pos1, pos2) ||> printfn "%d %d"
-            realRunGame (whoseTurn + 1, newDoubles, newOwner, pos2, newPos)
-                        p_two p_one
+   let newOwner = (
+      if (cubeOwner = -1 || cubeOwner % 2 = whoseTurn % 2) && (p_one.shouldDouble pos1 pos2) then 
+         if p_two.shouldTake pos2 pos1 then (whoseTurn + 1) else (cubeOwner)
+      else
+         cubeOwner
+   )
+
+   let playerMod =
+      match whoseTurn % 2 with
+         | 0 ->  1.0
+         | 1 -> -1.0
+         | _ ->  0.0
+
+   match newDoubles, pos (pos1 + (roll ())) with
+      | newDoubles, _ when newDoubles < doubles ->
+         // This is an indicator that playertwo rejected the bet
+         (pos1, pos2) ||> printfn "Rejected: %d %d"
+         2.0 ** float doubles * playerMod
+      | _, newPos when newPos >= 100 ->
+         (pos1, pos2) ||> printfn "Victory: %d %d"
+         2.0 ** float newDoubles * playerMod
+      | _, newPos ->
+         (pos1, pos2) ||> printfn "%d %d"
+         realRunGame (whoseTurn + 1, newDoubles, newOwner, pos2, newPos)
+                  p_two p_one
 
 let runGame = realRunGame (0,0,-1,0,0)
 

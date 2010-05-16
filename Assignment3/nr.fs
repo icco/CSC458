35535c6672f0e73469a48caf7f381ec70db90779
Nat Welch <nat@natwelch.com>
2010-05-16 12:28:54 -0700

    Basic layout for newton raphson.

--- Assignment3/nr.fs
+++ Assignment3/nr.fs
@@ -1,5 +1,25 @@
 type term = (double * double)
 type eqn = term list
 
-let nr = ((x : eqn), (y : double)) =
-   double
+(**
+ * Given an equation and a value, solve, return dbl.
+ *)
+let solve eq v = 1.0
+
+(**
+ * Given an equation, diffentiate and return eq'
+ *)
+let dx ( eq : eqn ) = eq
+
+(**
+ * x is the equation we want to estimate
+ * y is our guess
+ *
+ * basic equation:
+ *  new_guess = guess - f(guess) / f'(guess)
+ * 
+ * TODO: Figure out when to stop recursing...
+ *)
+let rec nr (x : eqn) (y : double) =
+   nr x (x - ((solve x y)/(solve (dx x) y))) 
+   

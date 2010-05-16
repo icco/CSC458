1e68ed265dfdf044d216573f6004b463597fe522
Nat Welch <nat@natwelch.com>
2010-05-16 12:57:38 -0700

    whee nr

--- Assignment3/nr.fs
+++ Assignment3/nr.fs
@@ -1,15 +1,20 @@
+
 type term = (double * double)
 type eqn = term list
 
 (**
  * Given an equation and a value, solve, return dbl.
  *)
-let solve eq v = 1.0
+let solve ( eq : eqn ) ( v : double ) = 1.0
 
 (**
  * Given an equation, diffentiate and return eq'
  *)
-let dx ( eq : eqn ) = eq
+let dx ( eq : eqn ) =
+   let rec loop acc = function
+      | [] -> acc
+      | ((f, e)::t) -> if e > 0.0 then (loop (((f*e), (e - 1.0)) :: acc) t); else acc
+   loop [] eq
 
 (**
  * x is the equation we want to estimate
@@ -21,5 +26,11 @@ let dx ( eq : eqn ) = eq
  * TODO: Figure out when to stop recursing...
  *)
 let rec nr (x : eqn) (y : double) =
-   nr x (x - ((solve x y)/(solve (dx x) y))) 
-   
+   (nr x (y - ((solve x y) / (solve (dx x) y))))
+
+
+(* Tests... *)
+ignore ( solve : eqn -> double -> double ) 
+ignore ( dx : eqn -> eqn ) 
+ignore ( nr : eqn -> double -> double ) 
+

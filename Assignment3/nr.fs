aecb02292f17a51823fd6d585989e94c2b7fa11d
Nat Welch <nat@natwelch.com>
2010-05-21 11:32:57 -0700

    Finish part one testing

--- Assignment3/nr.fs
+++ Assignment3/nr.fs
@@ -2,12 +2,21 @@
  * Assignment 3
  * @author Nathaniel "Nat" Welch
  *)
+module Ass3
+
+type term = (double * double)
+type poly = term list
+
+// val findZeros : poly * double -> double
 
 type event1 = bool
 type event = int -> event1
-type rv = event -> double // a random variable
-type term = (double * double)
-type poly = term list
+type rv = event -> double
+
+// val expectedVal : rv -> double -> double
+
+// Define our exception type
+exception StrEx of string
 
 (**
  * Given an equation and a value, solve, return a double.
@@ -30,13 +39,16 @@ let dx ( eq : poly ) =
  *
  * basic equation:
  *  new_guess = guess - f(guess) / f'(guess)
- * 
- * TODO: Figure out when to stop recursing...
  *)
 let rec findZeros_ ((x : poly), (y : double)) i =
+   let d = (dx x) // Take the derivative and store
+   let ex = StrEx "This function has a derivative of zero."
    if i = 0 then y
    else
-      findZeros_ (x, (y - ((solve x y) / (solve (dx x) y)))) (i-1)
+      if (d = []) then 
+         raise ex
+      else
+         findZeros_ (x, (y - ((solve x y) / (solve d y)))) (i-1)
 
 let rec findZeros ((x : poly), (y : double)) =
    findZeros_ (x, y) 1000
@@ -47,6 +59,7 @@ let rec findZeros ((x : poly), (y : double)) =
  * 
  * Assumes a discrete random variable
  *)
+(* DOES NOT WORK...
 let rec _expectedVal i ( r : rv ) ( d : double ) =
    if i = 1000 then 0.0
    else
@@ -54,13 +67,14 @@ let rec _expectedVal i ( r : rv ) ( d : double ) =
 
 let expectedVal ( r : rv ) ( d : double ) =
    _expectedVal 100 r d
+*)
 
 (* Tests... *)
 ignore ( solve : poly -> double -> double )
 ignore ( dx : poly -> poly )
 ignore ( findZeros : poly * double -> double )
 
-ignore ( expectedVal : rv -> double -> double )
+//ignore ( expectedVal : rv -> double -> double )
 
 let test _ =
    printfn "--- Test solve: "
@@ -73,9 +87,23 @@ let test _ =
    printfn "equals  0? %A" (dx ((4.0, 0.0)::[]))
    printfn "equals 3x2 + 2x + 4? %A" (dx ((1.0, 3.0)::(1.0, 2.0)::(4.0, 1.0)::[]))
    printfn "--- Test findZeros: "
+   let f = (1.0, 2.0)::(-612.0, 0.0)::[]
+   in
+      printfn "x^2 - 612 : 24.73863375 ? %A" ( findZeros (f, 10.0) )
+   let f = (1.0, 2.0)::(2.0, 5.0)::(-612.0, 0.0)::[]
+   in
+      printfn "x^2 + 2x^5 - 612 : 3.131419962 ? %A" ( findZeros (f, 10.0) );
+      printfn "x^2 + 2x^5 - 612 : 3.131419962 ? %A" ( findZeros (f, 1.0) )
+   let f = (6.0, 0.0)::[]
+   in
+      try
+         printf "6 : exception ? " ;
+         (printfn "6 : exception ? %A" ( findZeros (f, 10.0) ))
+      with
+         | exn ->
+            printf " yes."
+            //printf "Exception message: %s" exn.Message
 
-
-
-
-test () ;;
+// Uncomment to run tests
+//test () ;;
 

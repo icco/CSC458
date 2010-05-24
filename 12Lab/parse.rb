598313c751b759495657705f32926ce576221ac7
Nat Welch <nat@natwelch.com>
2010-05-24 09:56:03 -0700

    layout of lab

---
+++ 12Lab/parse.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+
+# open each stock in stocks.txt
+
+# parse the lines, regex with /^\(.*?,){4}(\d+\.\d+).*$/
+
+# calculate differences between days
+
+# take log of each, calculate std dev and mean
+
+# graph. risk vs return

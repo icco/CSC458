1d27d1a94e8a47e48cbc0c7921d09d7e926cbace
Nat Welch <nat@natwelch.com>
2010-05-28 00:05:35 -0700

    update pull script

--- 12Lab/get.sh
+++ 12Lab/get.sh
@@ -4,5 +4,7 @@ mkdir -p stocks
 
 for i in `cat stocks.txt`; do
    echo $i;
-   curl -s "http://ichart.finance.yahoo.com/table.csv?s=$i&d=4&e=24&f=2010&g=d&a=0&b=2&c=1962&ignore=.csv" > stocks/$i.csv
+   # curl -s "http://ichart.finance.yahoo.com/table.csv?s=$i&d=4&e=24&f=2010&g=d&a=0&b=2&c=1990&ignore=.csv" | perl -pe 's/^(.*?,){4}(\d+\.\d+).*$/$2/;' > stocks/$i.csv
+   curl -s "http://ichart.finance.yahoo.com/table.csv?s=$i&d=4&e=24&f=2010&g=d&a=0&b=2&c=1990&ignore=.csv" > stocks/$i.csv
 done;
+

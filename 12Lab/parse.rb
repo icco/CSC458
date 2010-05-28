2e448921390c60b94ad87d340b227ac3b6062d23
Nat Welch <nat@natwelch.com>
2010-05-28 00:26:42 -0700

    Return not mean

--- 12Lab/parse.rb
+++ 12Lab/parse.rb
@@ -1,16 +1,20 @@
 #!/usr/bin/ruby
 
-require 'rubygems'
-require 'google_chart' # sudo gem install gchart
+#require 'rubygems'
+#require 'google_chart' # sudo gem install gchart
 
 class Array
-   def mean
+   def return # Average Return
+      (inject(:*) ** (1.0/(size-1.0)))
+   end
+
+   def mean # Aritmetic
       inject(:+) / size.to_f
    end
 
    def std_dev
       m = mean; c = size
-      Math.sqrt( inject(0) { |sum, e| sum + (e - m) ** 2 } / c.to_f )
+      Math.sqrt( inject(0) { |sum, e| sum + ((e - m) ** 2) } / c.to_f )
    end
 end
 
@@ -23,26 +27,27 @@ stocks.map! { |stock|
    csv = File.new "stocks/#{stock}.csv"
    values = csv.readlines
    values.delete_at(0)
+   values.reverse!
    values.map! { |line|
-      line = line.strip.match(/^(.*?,){4}(\d+\.\d+).*$/)[2].to_f
+      line = line.strip.match(/^(.*?,){5}(\d+\.\d+).*$/)[2].to_f
    }
 
    # calculate differences between days
    diffs = []
    for i in 1..(values.length-1) do
-      diffs[i-1] = values[i] - values[i-1]
+      diffs[i-1] = values[i] / values[i-1]
    end
 
    # take log of each, calculate std dev and mean
-   diffs.map! { |val| if val.zero? then 0.0 else val = Math.log10 val.abs end }
+   #diffs.map! { |val| if val.zero? then 0.0 else val = Math.log10 val.abs end }
 
-   puts "#{stock}, #{diffs.std_dev}, #{diffs.mean}"
-   stock = [ diffs.std_dev, diffs.mean ]
+   puts "#{stock}, #{diffs.std_dev}, #{diffs.return}"
+   stock = [ diffs.std_dev, diffs.return]
 }
 
 # graph. risk vs return
-puts "\nChart Url"
-sc = GoogleChart::ScatterChart.new('400x400',"Risk vs Return")
-sc.data "Scatter Set", stocks
-puts sc.to_url
+#puts "\nChart Url"
+#sc = GoogleChart::ScatterChart.new('400x400',"Risk vs Return")
+#sc.data "Scatter Set", stocks
+#puts sc.to_url
 

c77e33d46245444e70f7dbd1e63b0c6bbfea16fd
Nat Welch <nat@natwelch.com>
2010-06-03 21:55:30 -0700

    Modify get and parse to pull in less data

--- Assignment5/get.sh
+++ Assignment5/get.sh
@@ -4,7 +4,6 @@ mkdir -p stocks
 
 for i in `cat stocks.txt`; do
    echo $i;
-   # curl -s "http://ichart.finance.yahoo.com/table.csv?s=$i&d=4&e=24&f=2010&g=d&a=0&b=2&c=1990&ignore=.csv" | perl -pe 's/^(.*?,){4}(\d+\.\d+).*$/$2/;' > stocks/$i.csv
-   curl -s "http://ichart.finance.yahoo.com/table.csv?s=$i&d=4&e=24&f=2010&g=d&a=0&b=2&c=1990&ignore=.csv" > stocks/$i.csv
+   curl -s "http://ichart.finance.yahoo.com/table.csv?s=$i&d=4&e=24&f=2010&g=d&a=0&b=2&c=2006&ignore=.csv" > stocks/$i.csv
 done;
 

8794af2dc23077271f1dd7258ebfebd6f4a1588f
Nat Welch <nat@natwelch.com>
2010-06-03 22:23:55 -0700

    Comments!

--- Assignment5/Final.fs
+++ Assignment5/Final.fs
@@ -7,24 +7,41 @@
 
 module Final
 
+(** throughout the code, we need some randomness *)
 let r = System.Random()
+
+(** a die that returns between -2.0 and 4.0 *)
 let roll _ = double (r.Next(-2, 4))
 
+(** given yesterdays fake data, create todays *)
 let scale ( x : double ) = x + ((roll()) * r.NextDouble())
 
+(**
+ * This does the real work for generating our fake data using tail recursion
+ * and the above scale function.
+ *)
 let rec genList acc x =
    if x = 0 then acc 
    else
       genList ((scale (List.head acc))::acc) (x - 1)
 
+(** return our data, and seriously, who uses Arrays? Bah. *)
 let generateData _ =  Array.ofList ( List.rev ( genList [100.0] 1000 ) )
 
+(**
+ * geometric mean, pretty straight forward. see wikipedia article if this
+ * confuses you.
+ *)
 let geometricMean (data : double list) = 
    (List.fold (fun acc x -> acc * x) 1.0 data) ** 
       (1.0 / (double (List.length data) - 1.0))
 
+(** 
+ * I have trouble beleiving stocks can grow more than one order of magnitude in
+ * three years.
+ *)
 let tooMuchGrowth ( data : double list ) = 
-   ( (data.[999] / data.[0]) > 7.0 )
+   ( (data.[999] / data.[0]) < 10.0 )
 
 let stddev ( data : double list ) =
    let mean = List.average data
@@ -52,9 +69,9 @@ let differences (data : double list) =
 //   let dataReturn = geometricMean data
 
 let rec evaluateList d = 
-   let r = ( tooMuchGrowth d )::[]
-   let count = List.fold (fun acc x -> if x then acc + 1; else acc) 0 r
-   count > ((List.length r) / 2)
+   let tests = ( tooMuchGrowth d )::[]
+   let count = List.fold (fun acc x -> if x then acc + 1; else acc) 0 tests
+   count > ((List.length tests) / 2)
 
 let evaluateData x = evaluateList ( List.ofArray x )
 
